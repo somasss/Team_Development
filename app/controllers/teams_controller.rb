2c997595da49e2763976c80c64ccb1f11e565e98
soma <ssyy20210101@gmail.com>
2022-06-02 17:48:51 +0900

    fix add authority_user

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,7 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_team, only: %i[show edit update destroy]
+  before_action :authority_user, only: %i[edit update destroy]
 
   def index
     @teams = Team.all
@@ -56,4 +57,10 @@ class TeamsController < ApplicationController
   def team_params
     params.fetch(:team, {}).permit %i[name icon icon_cache owner_id keep_team_id]
   end
+  def authority_user
+    unless current_user.id == @team.owner.id
+      redirect_to @team, notice:"権限がありません"
+    end
+  end
+  
 end
